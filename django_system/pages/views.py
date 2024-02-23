890469bb0fb7524d82b4449299dad4a67cb1c4bb
Cícero José <cicerosousa205@gmail.com>
2024-02-23 16:43:51 -0300

    more tests

--- django_system/pages/views.py
+++ django_system/pages/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request, *args, **kwargs):
     print(request.user)
-    return render(request, "home.html", {})
+    my_context = {
+        "title": "This is my h1 title, did you like it?",
+        "paragraph": "This is my paragraph, it came from the context, how awesome is that?",
+        "my_items": ['apple', "banana", 'mango', 'pie', "eggs", 'delicious', 'fruit']
+    }
+
+    
+
+    return render(request, "home.html", my_context)
